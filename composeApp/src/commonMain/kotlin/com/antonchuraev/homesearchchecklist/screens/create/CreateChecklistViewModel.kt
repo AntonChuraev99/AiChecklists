4f6ed03ed302dbc89935cea9d043e1618cd3b83f
antonchuraev <churaevanton@gmail.com>
2025-11-19 18:32:58 +0700

    add edit items logic

--- composeApp/src/commonMain/kotlin/com/antonchuraev/homesearchchecklist/screens/create/CreateChecklistViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/antonchuraev/homesearchchecklist/screens/create/CreateChecklistViewModel.kt
@@ -5,22 +5,62 @@ import androidx.lifecycle.viewModelScope
 import com.antonchuraev.homesearchchecklist.data.repository.ChecklistRepository
 import com.antonchuraev.homesearchchecklist.domain.model.Checklist
 import com.antonchuraev.homesearchchecklist.domain.model.ChecklistItem
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 
 class CreateChecklistViewModel(
     private val checklistRepository: ChecklistRepository
 ) : ViewModel() {
 
+    private val name: MutableStateFlow<String> = MutableStateFlow("")
 
-    fun onSaveClick(name: String, elements: List<String>) {
+    private val items: MutableStateFlow<List<ChecklistItem>> = MutableStateFlow(emptyList())
+
+    val screenState = combine(
+        items,
+        name
+    ){ items , name ->
+        return@combine ScreenState(
+            name = name,
+            items = items
+        )
+    }
+        .stateIn(
+            viewModelScope,
+            SharingStarted.WhileSubscribed(5000),
+            ScreenState()
+        )
+
+    fun onNameChange(newName: String){
+        name.value = newName
+    }
+
+    fun onAddItem(itemText: String){
+        items.value += ChecklistItem(itemText, false)
+    }
+
+    fun onDeleteItem(item: ChecklistItem){
+        items.value -= item
+    }
+
+    fun onSaveClick() {
         viewModelScope.launch {
             checklistRepository.addChecklist(
                 Checklist(
-                    name = name,
-                    items = elements.map { ChecklistItem(it, false) }
+                    name = screenState.value.name,
+                    items = screenState.value.items
                 )
             )
         }
     }
 
 }
+
+data class ScreenState(
+    val name: String = "",
+    val items: List<ChecklistItem> = emptyList(),
+)
