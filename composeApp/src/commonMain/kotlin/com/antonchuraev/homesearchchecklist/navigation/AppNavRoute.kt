22433c9bb9390c0f35f864a22bc157a1953b20be
antonchuraev <churaevanton@gmail.com>
2025-12-24 15:23:15 +0700

    rename

---
+++ composeApp/src/commonMain/kotlin/com/antonchuraev/homesearchchecklist/navigation/AppNavRoute.kt
@@ -0,0 +1,24 @@
+package com.antonchuraev.homesearchchecklist.navigation
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+sealed class AppNavRoute(val route: String) {
+    @Serializable
+    data object Onboarding : AppNavRoute("onboarding")
+    
+    @Serializable
+    data object Main : AppNavRoute("main")
+
+    @Serializable
+    sealed class CreateChecklistRoute(private val routeParam: String) : AppNavRoute(routeParam) {
+        @Serializable
+        data class CreateChecklist(val templateId: Int?) : CreateChecklistRoute("create_checklist")
+
+        @Serializable
+        data object Templates : CreateChecklistRoute("templates")
+    }
+
+    @Serializable
+    data object Debug : AppNavRoute("debug")
+}
