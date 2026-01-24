d12b609138241238361c8e3b2c6c956d320039f3
Anton Churaev <churaevanton@gmail.com>
2026-01-24 22:42:04 +0700

    build(android): bump version to 1.1 (versionCode 2)

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -91,8 +91,8 @@ android {
         applicationId = "com.antonchuraev.aichecklists"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
