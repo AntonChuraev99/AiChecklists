b34a9117c2df63009e51e2bdfaee611156420caf
Anton Churaev <churaevanton@gmail.com>
2026-01-31 10:47:25 +0700

    versionCode = 9         versionName = "1.7.1"

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -98,8 +98,8 @@ android {
         applicationId = "com.antonchuraev.aichecklists"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 8
-        versionName = "1.7"
+        versionCode = 9
+        versionName = "1.7.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
