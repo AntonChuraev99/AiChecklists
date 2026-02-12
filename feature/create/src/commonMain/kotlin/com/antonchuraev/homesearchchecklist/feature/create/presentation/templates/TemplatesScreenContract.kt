c960e7058b37ac3913f660ffee01fe3a1d8d522e
Anton Churaev <churaevanton@gmail.com>
2026-02-12 20:55:20 +0700

    move search to toolbar

--- feature/create/src/commonMain/kotlin/com/antonchuraev/homesearchchecklist/feature/create/presentation/templates/TemplatesScreenContract.kt
+++ feature/create/src/commonMain/kotlin/com/antonchuraev/homesearchchecklist/feature/create/presentation/templates/TemplatesScreenContract.kt
@@ -10,6 +10,7 @@ data class TemplatesScreenState(
     val categories: List<TemplateCategory> = emptyList(),
     val filteredCategories: List<TemplateCategory> = emptyList(),
     val searchQuery: String = "",
+    val isSearchActive: Boolean = false,
     val selectedTemplate: ChecklistTemplate? = null,
     val showPreviewDialog: Boolean = false,
     val isCreating: Boolean = false,
@@ -23,6 +24,7 @@ sealed interface TemplatesScreenIntent : Intent {
     data object OnCreateFromTemplate : TemplatesScreenIntent
     data object OnDismissError : TemplatesScreenIntent
     data class OnSearchQueryChange(val query: String) : TemplatesScreenIntent
+    data object OnToggleSearch : TemplatesScreenIntent
 
     // Bottom action buttons
     data object OnCreateManuallyClick : TemplatesScreenIntent
