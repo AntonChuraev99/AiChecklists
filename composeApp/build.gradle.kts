6820c3241fd5059d10baa632fcd0171841711b14
Anton Churaev <churaevanton@gmail.com>
2026-02-24 20:32:09 +0700

    versionCode = 19         versionName = "1.9.3"

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -108,8 +108,8 @@ android {
         applicationId = "com.antonchuraev.aichecklists"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 18
-        versionName = "1.9.2"
+        versionCode = 19
+        versionName = "1.9.3"
 
         testInstrumentationRunner = "com.antonchuraev.aichecklists.TestRunner"
 
