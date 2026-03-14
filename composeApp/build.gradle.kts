83c3df46654958198b4d61c01d40f705a01f343d
Anton Churaev <churaevanton@gmail.com>
2026-03-14 21:07:51 +0700

    chore: bump versionCode to 25

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -108,7 +108,7 @@ android {
         applicationId = "com.antonchuraev.aichecklists"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 24
+        versionCode = 25
         versionName = "1.11.2"
 
         testInstrumentationRunner = "com.antonchuraev.aichecklists.TestRunner"
