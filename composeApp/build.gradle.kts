decc1f56e648836a842738bbedb724e27923c52d
Anton Churaev <churaevanton@gmail.com>
2026-04-28 20:07:40 +0700

    versionCode = 34         versionName = "1.13.2"

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -111,8 +111,8 @@ android {
         applicationId = "com.antonchuraev.aichecklists"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 33
-        versionName = "1.13.1"
+        versionCode = 34
+        versionName = "1.13.2"
 
         testInstrumentationRunner = "com.antonchuraev.aichecklists.TestRunner"
 
