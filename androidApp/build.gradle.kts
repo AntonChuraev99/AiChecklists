4f4233a62b71a723664d4032fb6fe6b203d8cf1b
Anton Churaev <churaevanton@gmail.com>
2026-06-18 12:15:49 +0700

    chore(release): bump version to 1.17.1 (50)

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.antonchuraev.aichecklists"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 49
-        versionName = "1.17.0"
+        versionCode = 50
+        versionName = "1.17.1"
 
         testInstrumentationRunner = "com.antonchuraev.aichecklists.TestRunner"
 
