27dfd87f028662fa661bb337cde7193e18dc1297
Anton Churaev <churaevanton@gmail.com>
2026-07-09 16:49:58 +0700

    chore(release): bump versionCode to 63

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.antonchuraev.aichecklists"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 62
-        versionName = "1.17.12"
+        versionCode = 63
+        versionName = "1.17.13"
 
         testInstrumentationRunner = "com.antonchuraev.aichecklists.TestRunner"
 
