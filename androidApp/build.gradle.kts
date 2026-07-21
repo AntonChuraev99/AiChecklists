bc8ff27383162700c42087bc002cdec92aff159b
Anton Churaev <churaevanton@gmail.com>
2026-07-21 10:20:48 +0700

    chore(release): bump version to 1.18.1

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.antonchuraev.aichecklists"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 68
-        versionName = "1.18"
+        versionCode = 69
+        versionName = "1.18.1"
 
         testInstrumentationRunner = "com.antonchuraev.aichecklists.TestRunner"
 
